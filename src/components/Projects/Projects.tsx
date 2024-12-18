ef5d58b3df3fc28280f7804345e3e464c60f9257
VadimSel <selyaaa33@gmail.com>
2024-12-18 20:14:30 +0300

    fixed projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,7 +5,7 @@ import img_2 from "../../assets/img/projects_2.png";
 import img_3 from "../../assets/img/projects_3.png";
 import img_4 from "../../assets/img/projects_4.png";
 import img_5 from "../../assets/img/projects_5.png";
-import whiteArrow from '../../assets/icons/arrow-right-white.svg'
+import whiteArrow from "../../assets/icons/arrow-right-white.svg";
 
 export const Projects = () => {
 	const images = [img_1, img_2, img_3, img_4, img_5];
@@ -14,15 +14,16 @@ export const Projects = () => {
 		<div className={s.container}>
 			<h2>Наши проекты</h2>
 			<div className={s.content}>
-				<img src={img_1} alt="img" />
-				<img src={img_2} alt="img" />
-				<img src={img_3} alt="img" />
-				<img src={img_4} alt="img" />
-				<img src={img_5} alt="img" />
-        <div className={s.subTitle}>
-          <span>Досуговый центр</span>
-          <span>Подробнее<img src={whiteArrow} alt="img" /></span>
-        </div>
+				{images.map((image, i) => {
+					return <img key={i} src={image} alt="img" />;
+				})}
+				<div className={s.subTitle}>
+					<span>Досуговый центр</span>
+					<span>
+						Подробнее
+						<img src={whiteArrow} alt="img" />
+					</span>
+				</div>
 			</div>
 			<Button text="Все проекты" type="secondary" />
 		</div>
